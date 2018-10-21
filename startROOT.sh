e87e2289dd2c59154cba965d6cf8919fc239bcd7
Dirk Kruecker <dirk.kruecker@desy.de>
2018-10-21 20:56:26 +0200

    README ROOT instruction 2

---
+++ startROOT.sh
@@ -0,0 +1,4 @@
+source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
+export PATH=/home/ubuntu/anaconda3/envs/pytorch_p27/bin/:$PATH
+conda activate python2
+source /home/ubuntu/root/bin/thisroot.sh
